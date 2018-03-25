d7ac38c9f5ed4e0a87da51680dc2f8b7d0171809
Francisco Vena <fvena32@gmail.com>
2018-03-25 01:30:11 +0100

    Añadido menú de inicio

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
+const program = require('commander');
 const chalk = require('chalk');   // Colorea las salidas
 const clear = require('clear');   // Limpia el terminal
 const figlet = require('figlet'); // Crea un título ASCII
 
-const inquirer = require('./lib/inquirer');
+const init = require('./lib/init');
 
+
+// Limpia el terminal
 clear();
 
-console.log(
-  chalk.yellow(
-    figlet.textSync('DIDOR', { horizontalLayout: 'full' })
-  )
-);
 
-console.log(
-  chalk.green(
-    figlet.textSync('API GEN', { verticalLayout: 'full' })
-  )
-);
+// Muestra el saludo inicial
+console.log(chalk.yellow(figlet.textSync('DIDOR', { horizontalLayout: 'full' })));
+console.log(chalk.green(figlet.textSync('GENERATOR', { verticalLayout: 'full' })));
 
-const run = async () => {
-  try {
-    const options = await inquirer.askProjectDetails();
 
-    console.log(options);
-  } catch(err) {
-    if (err) {
-      console.log(err);
-    }
-  }
+// Opciones de nuestro CLI
+// didor -v
+// didor -h
+// didor init
+program
+  .version('0.0.1', '-v, --version')
+  .option('init', 'Inicializa un proyecto')
+  .option('option1', 'Opción 1')
+  .option('option2', 'Opción 2');
+
+// Menú de ayuda
+program.on('--help', function(){
+  console.log('');
+  console.log('  Ejemplos:');
+  console.log('');
+  console.log('    $ didor -v');
+  console.log('    $ didor init');
+  console.log('    $ didor option1');
+  console.log('    $ didor option2');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+
+// Compruebo la opción elegida
+if (program.init) {
+  init.run();
+} else if (program.option1) {
+  console.log('Opción 1')
+} else if (program.option2) {
+  console.log('Opción 2')
+} else {
+  program.help();
 }
-
-run();
